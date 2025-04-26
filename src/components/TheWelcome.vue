9fae0787b5a224c077f407b7f04a21ef7ccddb2c
daw-rv <drosso@redventures.com>
2025-04-26 16:48:44 -0400

    first commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="app-welcome">
+        <h1>Bienvenido al Creador de Perfumes</h1>
+        <p>Te haremos una serie de preguntas para entender mejor el tipo de perfume que deseas crear.</p>
+        <p>Con tus respuestas, diseñaremos una combinación única de ingredientes que refleje tu estilo y personalidad.</p>
+         <img src="../assets/logo.png" alt="logo">
+    </div>
+
+</template>
+
+
+<style scoped>
+.app-welcome {
+  text-align: center;
+}
+.app-welcome img{
+  width: 35%;
+  object-fit: contain;
+  border-radius: 25px;
+  margin-top: 2rem;
+}
+</style>
