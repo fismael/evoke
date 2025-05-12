6e7af33dced48b28f20f02bc07c47025e38e96ce
fismael <fismael026@gmail.com>
2025-05-12 01:55:03 -0400

    text wrap

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -6,7 +6,7 @@
     <div class="app-welcome">
         <div class="welcome-content">
             <h1>Bienvenido al Creador de Perfumes</h1>
-            <p style="text-wrap: pretty;">
+            <p>
               Te haremos una serie de preguntas para entender mejor el tipo de perfume que deseas crear.
               Con tus respuestas, diseñaremos una combinación única de ingredientes que refleje tu estilo y personalidad.</p>
             <img src="../assets/logo.png" alt="logo">
@@ -21,6 +21,9 @@
   padding: 2rem;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   width: 700px;
+}
+.app-welcome p {
+  text-wrap: pretty;
 }
 .app-welcome img{
   width: 35%;
